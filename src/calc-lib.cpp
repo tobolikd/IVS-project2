4d63a33be701a995f84c4873c9578d0a4d6edb51
David Tobolik <tobolikg@gmail.com>
2023-03-22 11:50:40 +0100

    new: math lib interface

--- src/calc-lib.cpp
+++ src/calc-lib.cpp
@@ -1,2 +1,36 @@
-#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <variant>
+#include <vector>
 
+#include "calc-lib.hpp"
+
+using namespace std;
+
+template<typename T>
+T add(T x, T y) {
+    return x + y;
+}
+
+template<typename T>
+T subtract(T x, T y) {
+    return x - y;
+}
+
+template<typename T>
+T multiply(T x, T y) {
+    return x * y;
+}
+
+template<typename T>
+T divide(T x, T y) {
+    if (y == 0)
+        throw std::runtime_error("Division by zero");
+    return x / y;
+}
+
+std::vector<std::variant<int, char, float>> parseExpression(std::string expression) {
+    std::vector<std::variant<int, char, float>> parsed;
+    return parsed;
+    //TODO
+}
