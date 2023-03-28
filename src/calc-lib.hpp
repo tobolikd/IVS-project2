a42ec65131cf92dd0b31ab5a421a78b33d8f86da
TimotejHalenar <thalenar@gmail.com>
2023-03-28 22:47:47 +0200

    feat: expression parser, basic math library interface
    Parser mostly works, but only supports few math symbols, expression evaluator not yet implemented.

--- src/calc-lib.hpp
+++ src/calc-lib.hpp
@@ -1,36 +1,80 @@
+/**
+ * @file calc-lib.hpp
+ * @author Timotej Halenár (xhalen00), David Tobolík (xtobol06)
+ * @brief Calculator math library header - IVS Project 2022/23 // VUT FIT
+ * @version 0.1
+ * @date 2023-03-28
+ * 
+ * 
+ */
+
 #ifndef IVS_MATH_LIB
 #define IVS_MATH_LIB
 
 #include <vector>
 #include <string>
 #include <variant>
+#include <algorithm>
+#include <iostream>
 
-namespace IVSMath {
-    template<typename T>
-    T add(T x, T y);
 
-    template<typename T>
-    T subtract(T x, T y);
+/**
+* @brief Token structure.
+* 
+*/
+typedef struct token
+{
+    int int_value;
+    float float_value;
+    int type;
+}token_t;
 
-    template<typename T>
-    T multiply(T x, T y);
+/**
+ * @brief Enumeration of token types.
+ * 
+ */
+enum token_types{
+    INT_T,
+    FLT_T,
+    ADD_T,
+    SUB_T,
+    MUL_T,
+    DIV_T
+};
 
-    template<typename T>
-    T divide(T x, T y);
+template<typename T>
+T add(T x, T y);
 
-    template<typename T>
-    T power(T x, int n);
+template<typename T>
+T subtract(T x, T y);
 
-    double root(double x, double n);
+template<typename T>
+T multiply(T x, T y);
 
-    template<typename T>
-    double sin(T x);
+template<typename T>
+T divide(T x, T y);
 
-    template<typename T>
-    double cos(T x);
+template<typename T>
+T power(T x, int n);
 
-    long long int factorial(unsigned int x);
+double root(double x, double n);
 
-    std::vector<std::variant<int, char, float>> parseExpression(std::string expression);
-}
+template<typename T>
+double sin(T x);
+
+template<typename T>
+double cos(T x);
+
+long long int factorial(unsigned int x);
+
+std::vector<token_t> parseExpression(std::string expression);
+
+int evaluateIntExpression(std::vector<token_t> expression);
+
+float evaluateFloatExpression(std::vector<token_t> expression);
+
+void print_token(token_t token);
+
 #endif //IVS_MATH_LIB
+
+//**********End of file calc-lib.hpp************
