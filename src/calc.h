7580e281feed7d9d04a0385e6cfddb869a52fd76
Jakub Štětina <37512119+startler03@users.noreply.github.com>
2023-04-19 14:05:27 +0200

    Working gui (#16)
    * new: math lib interface
    
    * feat: expression parser, basic math library interface
    
    Parser mostly works, but only supports few math symbols, expression evaluator not yet implemented.
    
    * feat: math library functions
    
    Added implementations for power, factorial, root functions.
    
    * added build dir
    
    * updated .gitignore
    
    * gui initial
    
    * CMakeLists for gui
    
    * feat: precedence parser and expression evaluator
    
    Added ability to evaluate expressions with +, -, *, / math symbols and paretheses while maintaining correct calculation order.
    
    * Change build system to qmake + connect buttons to inner logic
    
    * Removed old gui source codes
    
    * updated gitignore to include new gui source codes
    
    * change default QMAKE_PATH in Makefile
    
    * auto close parentheses at the end of expression
    
    * Keyboard input support
    
    * Square root -> Nth root
    
    sqrt() string was now replaced for the standard root symbol. Number before root
    symbol indicates the order of the root eg. 3√5 = cube root of 5. In case of no digit before the root
    symbol 2 is added automatically (square root is assumed).
    
    * Removed unnnecessary debug info
    
    * Added ANS button
    
    The `ANS` button stores the previous answer (for use in future
    calculations)
    
    * Decimal comma -> decimal point
    
    * Added mathlib to calc.cpp
    
    * Added doxygen style comments
    
    * fix: revert CMakeLists.txt to build tests
    
    * chore: refactor building gui
    
    ---------
    
    Co-authored-by: David Tobolik <tobolikg@gmail.com>
    Co-authored-by: TimotejHalenar <thalenar@gmail.com>

---
+++ src/calc.h
@@ -0,0 +1,109 @@
+/**
+ * @file calc.h
+ * @author Jakub Štětina
+ * @brief Header file for the calc class
+ * @version 0.1
+ * @date 2023-04-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef CALC_H
+#define CALC_H
+
+#include <QMainWindow>
+#include <QSignalMapper>
+#include <QKeyEvent>
+#include <string>
+#include <map>
+#include "ui_calc.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class calc; }
+QT_END_NAMESPACE
+
+class calc : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    calc(QWidget *parent = nullptr);
+    QSignalMapper* buttonToChar;
+    ~calc();
+private slots:
+    void updateUserInput(QString);
+    void clearUserInput();
+    void evalInput();
+    void clearInputStrings();
+    void deleteChar();
+    void retrieveAnswer();
+
+private:
+    Ui::calc *ui;
+    std::string userInputStr;
+    std::string mathlibInputStr;
+    std::string prevAnswer;
+    bool result;
+    unsigned int unclosedBrackets;
+    void updateUnclosedBrackets();
+    void addUnclocedBrackets();
+    void convertExpression();
+
+
+    /**
+     * @brief Defines press events for keyboard input
+     * 
+     * @param event Key event
+     */
+    void keyPressEvent(QKeyEvent *event) override
+    {
+        // Defines control buttons with non-functional roles
+        switch(event->key()){
+            case Qt::Key_Delete:
+                this->clearInputStrings();
+                return;
+            case Qt::Key_Backspace:
+                this->deleteChar();
+                return;
+            case Qt::Key_Equal:
+            case Qt::Key_Enter:
+            case Qt::Key_Return:
+                this->evalInput();
+                return;
+        }
+        
+        // Maps symbol buttons
+        std::map<int, std::string> keyToSymbol = {
+            {Qt::Key_0, "0"},
+            {Qt::Key_1, "1"},
+            {Qt::Key_2, "2"},
+            {Qt::Key_3, "3"},
+            {Qt::Key_4, "4"},
+            {Qt::Key_5, "5"},
+            {Qt::Key_6, "6"},
+            {Qt::Key_7, "7"},
+            {Qt::Key_8, "8"},
+            {Qt::Key_9, "9"},
+
+            {Qt::Key_ParenLeft,   "("},
+            {Qt::Key_ParenRight,  ")"},
+            {Qt::Key_Exclam,      "!"},
+            {Qt::Key_AsciiCircum, "^"},
+            {Qt::Key_Plus,        "+"},
+            {Qt::Key_Minus,       "-"},
+            {Qt::Key_Asterisk,    "*"},
+            {Qt::Key_Slash,       "/"},
+            {Qt::Key_C,           "C"}
+        };
+
+        // If key is not found
+        if(keyToSymbol.find(event->key()) == keyToSymbol.end()){
+            return;
+        }
+
+        // Print user input on screen
+        this->updateUserInput(QString::fromStdString(keyToSymbol[event->key()]));
+
+    }};
+#endif // CALC_H
